3522c781ae7c3722a1a3218248359b238f349657
Gaurav Singh <gauravcool.680@gmail.com>
2025-11-02 21:50:01 +0530

    Added question description in SQL file

--- american_cities_over_100k.sql
+++ american_cities_over_100k.sql
@@ -1,2 +1,9 @@
-SELECT * FROM CITY
-where population > 100000 and Countrycode = 'USA';
+-- Question:
+-- Query all columns for all American cities in the CITY table 
+-- with populations larger than 100000.
+-- The CountryCode for America is USA.
+
+SELECT *
+FROM CITY
+WHERE CountryCode = 'USA'
+  AND Population > 100000;

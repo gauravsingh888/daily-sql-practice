90c4f44304347374b0191f8a8f895959b17d3b08
Gaurav Singh <gauravcool.680@gmail.com>
2025-11-16 08:37:51 +0530

    Added new SQL problem solution

---
+++ Average Population.sql
@@ -0,0 +1,6 @@
+--QUESTION
+--Query the average population for all cities in CITY, rounded down to the nearest integer.
+
+
+SELECT FLOOR(AVG(POPULATION))
+FROM CITY;
---
+++ Population_Density_Difference.sql
@@ -0,0 +1,6 @@
+--QUESTION
+--Query the difference between the maximum and minimum populations in CITY.
+
+
+SELECT MAX(POPULATION) - MIN(POPULATION) AS population_difference
+FROM CITY;
